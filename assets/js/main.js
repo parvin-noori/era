4970dde906103686b0041cb03fbf955cc678c1b1
Parvin Noori <parvinnoori0@gmail.com>
2025-02-09 15:18:57 +0330

    refactor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,67 @@
 $(document).ready(function () {
-  new fullpage('#fullpage', {
-    //options here
-    autoScrolling:true,
-    scrollHorizontally: true,
-    sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
-    anchors:['firstPage', 'secondPage'],
-    dragAndMove: {enable:true},
+  const eraSections = $(".era-section");
+
+  eraSections.each(function () {
+    const eraSection = $(this);
+
+    activeSectionPosition(eraSection);
+    configNextElement(eraSection);
+    configPrevElement(eraSection);
+    configSectionPositions(eraSection);
   });
+
+  function activeSectionPosition(element) {
+    element.addClass("inset-20");
+  }
+
+  function configNextElement(element) {
+    if (element.hasClass("active")) {
+      let nextElement = element.next();
+
+      while (nextElement.length) {
+        nextElement.addClass("after");
+        nextElement = nextElement.next();
+      }
+    }
+  }
+
+  function configPrevElement(element) {
+    if (element.hasClass("active")) {
+      let prevElement = element.prev();
+
+      while (prevElement.length) {
+        prevElement.addClass("before");
+        prevElement = prevElement.prev();
+      }
+    }
+  }
+
+  function configSectionPositions(element) {
+    if (element.hasClass("horiz")) {
+      applyTranslateToHorizSections();
+    } else {
+      applyTranslateToVerticalSections();
+    }
+  }
+
+  function applyTranslateToHorizSections() {
+    const $eraSections = $(".era-section.horiz");
+
+    if ($eraSections.length) {
+      $eraSections.first().addClass("translate-x-[101%]");
+      $eraSections.last().addClass("-translate-x-[101%]");
+    }
+  }
+
+  function applyTranslateToVerticalSections() {
+    const $eraSections = $(".era-section.vertical");
+
+    if ($eraSections.length) {
+      // $eraSections.first().addClass("translate-y-[102%]");
+      // $eraSections.last().addClass("-translate-y-[102%]");
+    }
+  }
+
+  var windowHeight = window.innerHeight;
+  console.log(windowHeight);
 });
